ae9eb715fb52b3fbee82ab57b1d2fc06e29ef506
Gorbunovski <ngorbunov520@gmail.com>
2026-07-18 19:57:22 +0300

    Added screen cleaning and changing load values every 1 second

--- script/specifications.py
+++ script/specifications.py
@@ -1,3 +1,9 @@
 import psutil
-print(f"Загрузка CPU: {psutil.cpu_percent()}%")
-print(f"Использование RAM: {psutil.virtual_memory().percent}%")
+import time
+import os
+
+while True:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(f'Загрузка CPU: {psutil.cpu_percent()}%')
+    print(f'Использование RAM: {psutil.virtual_memory().percent}%')
+    time.sleep(1)
