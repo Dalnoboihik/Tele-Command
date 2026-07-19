f14eeaa0e02a94bdd3eebd372be9ce442c24cd08
Gorbunovski <ngorbunov520@gmail.com>
2026-07-19 11:26:24 +0300

    The project's root folder has been cleared of unnecessary files

--- script/shutdown.py
+++
@@ -1,9 +0,0 @@
-import os
-
-KeyBoard = (input("1- Выключение, 2- Перезапуск 1/2: "))
-
-if KeyBoard == ("1"):
-    os.system("shutdown /s /t 0") 
-
-elif KeyBoard == ("2"):
-    ("shutdown -r") 
--- script/specifications.py
+++
@@ -1,9 +0,0 @@
-import psutil
-import time
-import os
-
-while True:
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print(f'Загрузка CPU: {psutil.cpu_percent()}%')
-    print(f'Использование RAM: {psutil.virtual_memory().percent}%')
-    time.sleep(1)
